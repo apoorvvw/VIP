f25707cc46b4547b15dc57591b7168af2bfe7595
apoorvvw <awairaga@purdue.edu>
2015-10-01 20:32:39 -0400

    Initial Commit
    Loading the files used to learn d3js

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php phpinfo(); ?>
+
+
+<!DOCTYPE HTML>
+<html >
+
+<style>
+body {
+    background-color:#FFFDDD ;
+}
+
+</style>
+
+
+<head>
+<meta charset="utf-8">
+<title> Police Form </title> 
+
+<script type="text/javascript" src="http://code.jquery.com/jquery.min.js">
+</script>
+
+</head>
+
+<body>
+  
+
+<div id="header" >
+<h1 style="margin-bottom:0;">Lafayette Indiana Police Department</h1></div>
+
+ 
+
+<br>
+<form action="index.php" method="get">
+<strong>Enter the Officer ID: </strong><input type="text" name="id"><br><br>
+   
+<input type="submit" >
+</form>
+
+</body>
+</html>
